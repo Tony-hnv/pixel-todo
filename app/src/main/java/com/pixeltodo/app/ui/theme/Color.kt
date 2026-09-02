62634e9ead00c9996747b4306454012427e1574e
浅吟清风Tony-hnv <54225824+Tony-hnv@users.noreply.github.com>
2026-09-02 08:03:28 +0800

    Add app/src/main/java/com/pixeltodo/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/pixeltodo/app/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.pixeltodo.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Pixel Art Color Palette
+val PixelBlue = Color(0xFF4A90D9)
+val PixelYellow = Color(0xFFFFD93D)
+val PixelBackground = Color(0xFF1A1A2E)
+val PixelCardBackground = Color(0xFF16213E)
+val PixelText = Color(0xFFEAEAEA)
+val PixelGreen = Color(0xFF6BCB77)
+val PixelRed = Color(0xFFFF6B6B)
+val PixelPurple = Color(0xFF9B59B6)
+val PixelOrange = Color(0xFFE67E22)
+val PixelGray = Color(0xFF95A5A6)
+val PixelDarkGray = Color(0xFF2C3E50)
